f5d740f2ca00397e6df98df74d935b47bf758166
Szymon Barczyk <szymonbarczyk@icloud.com>
2025-12-03 20:49:22 +0100

    Initial project setup

---
+++ src/main/java/pl/edu/agh/to/ToZtpApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.to;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToZtpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToZtpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/edu/agh/to/ToZtpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.to;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToZtpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
