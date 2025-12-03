0ff33bc35c5ad799583f0d184bdf7dd1f36585dd
Szymon Barczyk <szymonbarczyk@icloud.com>
2025-12-03 21:52:47 +0100

    Other small structure changes

--- src/main/java/pl/edu/agh/to/controller/TransportController.java
+++ src/main/java/pl/edu/agh/to/controller/TransportController.java
@@ -1,13 +1,17 @@
 package pl.edu.agh.to.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class TransportController {
 
     @GetMapping("/")
     public String home() {
         return "Transport service is running!";
     }
+
+
 }
---
+++ src/main/java/pl/edu/agh/to/service/GTFSService.java
@@ -0,0 +1,7 @@
+package pl.edu.agh.to.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GTFSService {
+}
